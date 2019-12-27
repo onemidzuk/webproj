e458efa87ad6bc903ec7575a2e8891ec08d5bf6a
onemidzuk <kuzneca@gmail.com>
2019-12-27 23:54:50 +0300

    coninue

--- we.py
+++ we.py
@@ -1,4 +1,6 @@
 import web
+import random
+
 render = web.template.render('templates/')
 urls = (
     '/', 'index',
@@ -7,18 +9,35 @@ urls = (
 
 db = web.database(dbn='postgres', user='exec', pw='webproj', db='webproj')
 
+
+
 class index:
+
+    r_old = -1
     def GET(self):
 
         # return render.index(i.name)
         todos = db.select('todo')
-        return render.index(todos)
+
+        while True :
+            r = random.randint(0, len(todos))
+            if r != r_old.index:
+                r_old = r
+                break
+        # return render.index(todos)
+        a = (todos[r]['title'])
+        return render.index(a)
+
+    def randomize(self):
+        r = random.randint(0, len(todos))
+        return r
 
 class add:
     def POST(self):
         i = web.input()
         n = db.insert('todo', title=i.title)
         raise web.seeother('/')
+        #return index.GET(self)
 
 if __name__ == "__main__":
     app = web.application(urls, globals())
